3c674b3bc3c77c62d91b64acb7f8255dc69b5b83
Kxkee <tom.cousin06@gmail.com>
2023-04-14 17:32:28 +0200

    project

--- app/api/cryptos/route.ts
+++ app/api/cryptos/route.ts
@@ -5,7 +5,7 @@ export async function GET(req: Request) {
     try {
         const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=400&convert=USD', {
             headers: {
-                'X-CMC_PRO_API_KEY': '6258727e-9671-4a43-ad72-8ab65a926a96',
+                'X-CMC_PRO_API_KEY': `cmc_api_key`,
             }
         })
         return NextResponse.json(response.data);
